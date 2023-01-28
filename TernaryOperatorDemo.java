2f3c653c3c74762be188518573c296d829b04358
GPratik18 <pratikghute18@gmail.com>
2023-01-28 23:26:41 +0545

    commit2 28JAN2023

---
+++ TernaryOperatorDemo.java
@@ -0,0 +1,11 @@
+package com.example.PWDemo;
+
+public class TernaryOperatorDemo {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		System.out.println((a<=b)?(a<b)?"a is smallest":"b is equal to a":"b is smallest");
+	}
+
+}
